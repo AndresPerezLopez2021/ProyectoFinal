a8e222128f7c6f3c95e123596a3879d7661c8cf9
AndresPerezLopez2021 <92432494+AndresPerezLopez2021@users.noreply.github.com>
2021-11-19 21:12:07 +0100

    Comienzo con Conexión al Back

--- peludo/src/main/resources/js/index.js
+++ peludo/src/main/resources/js/index.js
@@ -35,6 +35,7 @@ function comprobacionFormulario() {
     formularioApellido();
     formularioRaza();
     formularioFecha();
+    formularioLocalidad();
     formularioAreaTexto();
     console.log("formulario");
     console.log($errores)
@@ -71,16 +72,14 @@ function formularioApellido() {
     }
 }
 function formularioRaza() {
-    //nombre, le añadimos un atributo al html
+    //Raza, le añadimos un atributo al html
     $("#razaFormContac").attr('required', true);
     if ($("#razaFormContac").val() == "") {
         //Almacenamos los errores
         err += 1;
-        $errores.push(new CampoErroneo(`razaFormContac ${err}`, `Debe estar cubierto la Raza`));
+        $errores.push(new CampoErroneo(`razaFormContac ${err}`, `Debe estar cubierta la Raza`));
     }
 }
-
-
 function formularioFecha() {
     $("#fechaFormContac").attr('required', true);
     let patronFecha = /^([12]\d{3})[\/\-](((0)[0-9])|((1)[0-2]))[\/\-]([0-2][0-9]|(3)[0-1])$/;
@@ -89,6 +88,15 @@ function formularioFecha() {
     if (!campoCorrectoFecha) {
         err += 1;
         $errores.push(new CampoErroneo(`fechaFormContac ${err}`, `El formato de fecha tiene que ser aaaa-mm-dd ej: 1985/08/01`));
+    }
+}
+function formularioLocalidad(){
+
+    $("#localidadFormContac").attr('required', true);
+    if ($("#localidadFormContac").val() == "") {
+        //Almacenamos los errores
+        err += 1;
+        $errores.push(new CampoErroneo(`localidadFormContac ${err}`, `Debe estar cubierta la localidad`));
     }
 }
 function formularioAreaTexto(){
@@ -100,7 +108,63 @@ function formularioAreaTexto(){
         }
 
 }
+//FOrmulario de identificación
+function formularioIdentificacion() {
+    $errores = [];
+    err = 0;
+    console.log($errores)
+    correoLogin();
+    correoConfirmLogin();
+    contrasenha();
+    console.log("formulario");
+    console.log($errores)
 
+    if (err == 0) {
+        alert("Envio Correcto");
+
+        onRequestHandler();
+    } else {
+        errorMensaje = "hay errores!!\n"
+        $errores.map(function(obj) {
+            console.log(obj.mensajeE)
+            return errorMensaje += obj.mensajeE + "\n"
+        })
+        confirm(errorMensaje);
+    }
+}
+function correoLogin() {
+    var re = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    var emailOK = document.getElementById("email");
+    var eOK = emailOK.value.match(re); //Devolve null se non cumpre a expresión regular "re"...
+    if (!eOK) {
+        console.log(emailOK.value + ' El campo de emamil no es correcto');
+        err += 1;
+        $errores.push(new CampoErroneo("email", `${err}: El campo de emamil no es correcto`))
+    }
+}
+function correoConfirmLogin() {
+    var emailOK = document.getElementById("email");
+    var confemailOK = document.getElementById("emailConfirm");
+    if (emailOK.value != confemailOK.value) {
+        eOK = null
+    } else {
+        eOK = !null
+    };
+    console.log(eOK);
+    if (!eOK) {
+            console.error(confemailOK.value + '  El campo de confirmación no es correcto');
+        err += 1;
+        $errores.push(new CampoErroneo("emailConfirm", `${err}: El campo de confirmación no es correcto`))
+    }
+}
+function contrasenha(){
+$("#password").attr('required', true);
+    if ($("#password").val() == "") {
+        //Almacenamos los errores
+        err += 1;
+        $errores.push(new CampoErroneo(`password ${err}`, `Debe estar cubierto la contraseña`));
+    }
+}
 //Se crea un filter para las imagenes publicadas de este modo el usuario puede visualizar la raza que esté interesada
 
 function cargarGaleria(){
@@ -117,5 +181,22 @@ function cargarGaleria(){
         $(this).removeClass("active");
     }
     $(this).addClass("active")
+
+}
+
+//REQUEST
+
+const xhr = new XMLHttpRequest();
+function onRequestHandler(){
+if ( this.readyState == 4 && this.status == 200){
+    const data = JSON.parse(this.response);
+    console.log(data);
+    //Pruebas
+   // const HTMLResponse = document.querySelector ('#request');
+   // const mapaUser = data.map((user)=> '<li>${user.name}');
+}
+xhr.addEventListener("load", onRequestHandler);
+xhr.open('GET', 'http://localhost:8080/animals');
+xhr.send();
 
 }
---
+++ peludo/src/main/resources/js/xmlHttpRequest.js
@@ -0,0 +1,15 @@
+//XMLHttRequest
+
+const xhr = new XMLHttpRequest();
+function onRequestHandler(){
+if ( this.readyState == 4 && this.status == 200){
+    const data = JSON.parse(this.response);
+    console.log(data);
+    const HTMLResponse = document.querySelector ('#request');
+    const mapaUser = data.map((user)=> '<li>${user.name}');
+}
+xhr.addEventListener("load", onRequestHandler);
+xhr.open('GET', 'http://localhost:8080/animals');
+xhr.send();
+
+}
